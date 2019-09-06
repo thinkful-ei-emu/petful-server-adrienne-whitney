40fd3362a7ca9673e243b7390da6c69672d91342
Adrienne Smith <agsmith046@gmail.com>
2019-09-06 11:45:39 -0500

    fixed server issue, started working on get endpoint

---
+++ Petful-server/src/people_list.js
@@ -0,0 +1,62 @@
+// had thought that could have people have criteria for type of pet looking for and then if they do not match up with the animal in line they go to the back of the queue?
+
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+    if(this.first == null) {
+      this.first = node;
+    }
+    if(this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
+  }
+
+  dequeue() {
+    if(this.first == null) {
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
+    if(node === this.last) {
+      this.last == null;
+    }
+    return node.value;
+  }
+}
+
+function peek(queue) {
+  if(queue.last == null) {
+    return 'No more pets available for adoption';
+  }
+  return queue.last.value;
+}
+
+function isEmpty(queue) {
+  if(queue.first == null) {
+    return 'No more pets available for adoption';
+  }
+}
+
+function display(queue) {
+  let string = '';
+  let tempNode = queue.first;
+  while (tempNode !== null) {
+    string = string + tempNode.value + ' ';
+    tempNode = tempNode.next;
+  }
+  console.log(string);
+  return string; // because needs to actually show up on client side right?
+}
--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const {NODE_ENV} = require('./config');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+
+const serializeCat = cat => ({
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+
+// const serializeDog = dog => ({
+//   imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+//   imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+//   name: 'Zeus',
+//   sex: 'Male',
+//   age: 3,
+//   breed: 'Golden Retriever',
+//   story: 'Owner Passed away'
+// });
+
+app
+  .get('/api/cat', (req, res, next) => {
+    res.json(serializeCat);
+  });
 
 // Catch-all 404
 app.use(function (req, res, next) {
@@ -14,6 +42,10 @@ app.use(function (req, res, next) {
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
+  let response;
+  if(NODE_ENV === 'production') {
+    response = {error: {message: 'server error'}};
+  }
   res.status(err.status || 500);
   res.json({
     message: err.message,
