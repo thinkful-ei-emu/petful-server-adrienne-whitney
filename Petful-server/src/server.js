1addca50669c7fab216643106c7c2fa70f2aafcb
Adrienne Smith <agsmith046@gmail.com>
2019-09-06 13:43:25 -0500

    get endpoints working

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const {NODE_ENV} = require('./config');
+const cats = require('./pets/cats.json');
+const dogs = require('./pets/dogs.json');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const serializeCat = cat => ({
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-});
-
-// const serializeDog = dog => ({
-//   imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-//   imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-//   name: 'Zeus',
-//   sex: 'Male',
-//   age: 3,
-//   breed: 'Golden Retriever',
-//   story: 'Owner Passed away'
-// });
-
 app
   .get('/api/cat', (req, res, next) => {
-    res.json(serializeCat);
+    res.json(cats);
+  });
+
+app
+  .get('/api/dog', (req, res, next) => {
+    res.json(dogs);
   });
 
 // Catch-all 404
